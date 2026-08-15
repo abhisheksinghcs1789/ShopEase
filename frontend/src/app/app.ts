f2ee2a9a7f9efa9996d17a7044b31541e8075c02
Abhishek Singh <abhisheksingh.cs1789@gmail.com>
2026-08-15 20:48:40 +0530

    update navbar

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { Navbar } from './layout/navbar/navbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
